179173af50d9a641b30d9d4d44a24fb3d2f897f4
Sumonche016 <sumonche016@gmail.com>
2022-01-14 19:01:14 +0600

    update the README.md with link to contributing guide

---
+++ app.js
@@ -0,0 +1,30 @@
+// this is for nav bar 
+
+window.addEventListener('scroll', function() {
+
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+
+})
+
+
+// this is for tabs
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+
+})

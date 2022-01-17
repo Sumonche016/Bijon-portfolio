287ef6a943868912148af88bc3f23f225863ecb4
Sumonche016 <sumonche016@gmail.com>
2022-01-17 21:08:33 +0600

    first commit

--- app.js
+++ app.js
@@ -28,3 +28,27 @@ tabs.forEach(tab => {
     })
 
 })
+
+
+
+
+
+// const sections = document.querySelectorAll("section");
+// const navLi = document.querySelectorAll("left__list-item");
+// window.addEventListener("scroll", () => {
+//   let current = "";
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop;
+//     const sectionHeight = section.clientHeight;
+//     if (pageYOffset >= sectionTop - sectionHeight / 3) {
+//       current = section.getAttribute("id");
+//     }
+//   });
+
+//   navLi.forEach((li) => {
+//     li.classList.remove("active");
+//     if (li.classList.contains(current)) {
+//       li.classList.add("active");
+//     }
+//   });
+// });
